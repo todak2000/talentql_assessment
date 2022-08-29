6308d7cb61a3e3ed5fea5b67ed3b05a737c8882a
Daniel Olagunju <todak2000@gmail.com>
2022-08-29 22:35:55 +0100

    fixing next button test bug

--- src/api.js
+++ src/api.js
@@ -4,10 +4,9 @@ const apiKey = "8csrgnjw?key=LEIX-GF3O-AG7I-6J84";
 let add;
 let previousNumber = 1;
 export async function getData(data) {
-    if (data % 2 == 1) { add = "&page="+data }
-    else if (data % 2 == 0) {
-        if (data === previousNumber){
-            add = ""}
+    if ( data % 2 == 1 ) { add = "&page="+data }
+    else if ( data % 2 === 0 ) {
+        if ( data === previousNumber ){ add = "" }
         else {
             add = "&page="+data
             previousNumber = data
