668f9e7e940eecff34ed2d62dbaf0a29881cb148
Daniel Olagunju <todak2000@gmail.com>
2022-08-30 01:33:42 +0100

    fixing codacy issues

--- src/api.js
+++ src/api.js
@@ -5,14 +5,13 @@ let add;
 let previousNumber = 1;
 export async function getData(data) {
     if ( data % 2 == 1 ) { 
-        add = "&page="+data }
-    else if ( data % 2 === 0 
-        ) {
-        if ( data === previousNumber 
-            ){ add = "" }
+        add = "&page="+`${data}`; }
+    else if ( data % 2 === 0 ) {
+        if ( data === previousNumber ){ 
+            add = ""; }
         else {
-            add = "&page="+data;
-            previousNumber = data
+            add = "&page="+`${data}`;
+            previousNumber = data;
         }
         
     }
